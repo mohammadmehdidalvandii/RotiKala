d1dacd5ac37cd4f811401b2d3f76468e46d9d597
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-02-08 16:18:05 +0330

    fix style productCard

--- src/components/module/ProductCard/ProductCard.jsx
+++ src/components/module/ProductCard/ProductCard.jsx
@@ -5,8 +5,10 @@ function ProductCard({image , name ,price}) {
   return (
                  <NavLink to='/Product-D' className="productCard">
                     <img src={image} alt="product" className="productCard_img" />
+                    <div className="productCard_content">
                     <h6 className="productCard_title">{name}</h6>
                     <span className="productCard_price">{price} تومان</span>
+                    </div>
                 </NavLink>
   )
 }
